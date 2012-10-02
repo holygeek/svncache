d68c98c2c95247095a588c2b96dd153a5d503ae2
nazri <nazri.ramliy@nsn.com>
2012-10-02 09:52:03 +0800

    read.pl - Show log in a format similar to the one produced by svn log -v

---
+++ SVNLog.pm
@@ -0,0 +1,25 @@
+package SVNLog;
+use strict;
+use warnings;
+
+sub print_cached_log {
+  my ($l) = @_;
+  # $l is the revision entries dumped as retrieved from Svn::Log::retrive();
+
+  my $rev_and_date = "r$l->{revision} | $l->{author} | $l->{date} +0800 (Day, DD Mon YYYY) | ? line";
+  print "------------------------------------------------------------------------\n";
+  print "$rev_and_date\n";
+  foreach my $path (sort keys %{$l->{paths}}) {
+    print $l->{paths}->{$path}->{action} . " " . $path . "\n";
+  }
+  print "\n";
+  print $l->{message};
+  print "------------------------------------------------------------------------\n";
+}
+
+sub get_dir_for {
+	my ($r) = @_;
+	return int($r / 1000);
+}
+
+1;
--- read.pl
+++ read.pl
@@ -2,6 +2,15 @@ use strict;
 use warnings;
 use Data::Dumper;
 use File::Slurp;
+use SVNLog;
 
-my $r = do { no strict 'vars'; eval read_file($ARGV[0]); };
-print Dumper $r;
+my $uuid = $ARGV[0] or die "No uuid given.";
+my $r = $ARGV[1] or die "No revision given.";
+
+my $dir = SVNLog::get_dir_for($r);
+$dir = "$uuid/$dir";
+my $cached = "$dir/$r";
+
+my $l = do { no strict 'vars'; eval read_file($cached); };
+
+SVNLog::print_cached_log($l);
