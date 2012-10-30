468fec8f4a1ea843b8f5d4f61e04dd6ad19165fb
nazri <nazri.ramliy@nsn.com>
2012-10-30 16:05:18 +0800

    read.pl: bugfix revision range did not work

--- read.pl
+++ read.pl
@@ -6,15 +6,21 @@ use SVNLog;
 my $uuid = $ARGV[0] or die "No uuid given.";
 my $rev_from = $ARGV[1] or die "No revision given.";
 my $rev_to   = $ARGV[2] || $rev_from;
+if ($rev_to eq 'MAX') {
+  $rev_to = SVNLog::get_latest_revision($uuid);
+}
+if ($rev_from eq 'MAX') {
+  $rev_from = SVNLog::get_latest_revision($uuid);
+}
 
-#print "A $rev_from $rev_to\n";
 foreach my $r ($rev_from..$rev_to) {
-  my $dir = SVNLog::get_dir_for($rev_from);
+
+  my $dir = SVNLog::get_dir_for($r);
   $dir = "$uuid/$dir";
-  my $cached = "$dir/$rev_from";
+  my $cached = "$dir/$r";
 
   if ( ! -f $cached ) {
-    print "$0: Revision not cached: $rev_from\n";
+    print "$0: Revision not cached: $r\n";
     exit 1;
   }
 
@@ -22,4 +28,3 @@ foreach my $r ($rev_from..$rev_to) {
 
   SVNLog::print_cached_log($l);
 }
-#print "B\n";
